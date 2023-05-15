3889212cb17598c7228dae2575151862b383f984
Gloria <ubahgloria16@gmail.com>
2023-05-15 14:49:03 +0100

    Feat: Complete setup

---
+++ LendSqr/src/components/Login.tsx
@@ -0,0 +1,10 @@
+
+const Login = () => {
+     return (
+          <section>
+               <p className={`font-face-sc`}>Hello world: Login</p>
+          </section>
+     );
+}
+
+export default Login;
---
+++ LendSqr/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#FFF",
+        primaryColor: "#213F7D",
+        textColor: "#545F7D",
+        inputFeld: "#545F7D",
+        secondaryColor: "#39CDCC",
+        sidebarLinks: "#213F7D",
+        blacklist: "#E4033B",
+        pending: "#E9B200",
+        active: "#39CD62",
+        navigate: "rgba(33, 63, 125, 0.1)"
+      },
+      fontFamily: {
+        'Roboto':  ("sans-serif"),
+        'Work Sans': ("sans-serif")
+      },
+    },
+  },
+  plugins: [],
+}
+
