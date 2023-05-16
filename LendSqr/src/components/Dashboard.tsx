0ec920a8219e5e4769d20b4c305fa937b607f4f2
Gloria <ubahgloria16@gmail.com>
2023-05-16 16:22:51 +0100

    Chore: Fixed the toggle menu

--- LendSqr/src/components/Dashboard.tsx
+++ LendSqr/src/components/Dashboard.tsx
@@ -8,18 +8,21 @@ import { Link } from "react-router-dom";
 import CustomerData from "../lib/customers";
 import BusinessData from "../lib/business";
 import SettingsData from "../lib/settings";
+import { useState } from "react";
 
 const Dashboard = () => {
+const [toggle, setToggle] = useState(false);
+
   return (
     <section>
       <div>
         <div
-          className={`h-[100px] p-4 flex justify-between items-center shadow-sm fixed w-full right-0 top-0 left-0 z-20`}
+          className={`h-[100px] p-4 flex justify-between items-center shadow-sm fixed w-full right-0 top-0 left-0 z-40`}
         >
-          <div className={`w-20`}>
+          <div className={`w-16 md:w-[20%]`}>
             <img src={Logo} alt="" />
           </div>
-          <div className={`relative w-48 hidden`}>
+          <div className={`relative w-48 hidden md:block md:w-[40%]`}>
             <input
               type="text"
               name=""
@@ -38,7 +41,7 @@ const Dashboard = () => {
           <div className={`flex justify-between items-center w-[14rem]`}>
             <a
               href="docs"
-              className={`text-primaryColor font-Roboto text-[16px] underline leading-[19px] mr-3`}
+              className={`text-primaryColor font-Roboto text-[16px] underline leading-[19px] md:mr-3`}
             >
               Docs
             </a>
@@ -61,17 +64,35 @@ const Dashboard = () => {
           </div>
         </div>
 
-        <div className={``}>
-          {/* bg-[#3f3d3d52] relative overflow-hidden top-[100px] bottom-0 right-0 left-0 z-0 */}
           <div
-            className={`bg-[rgb(255,255,255)] fixed top-[100px] bottom-0 left-0 z-10 p-4 overflow-y-scroll`}
+            className={`${toggle ? `w-[240px]` : `w-[60px]`} fixed overflow-y-scroll top-[100px] bottom-0 left-0 z-10`}
           >
-            <div>
-              <div className={`flex items-center mt-4 mb-10`}>
+          <MdArrowDropDown
+              className={`${toggle ? `left-[220px] rotate-90` : `left-[40px] -rotate-90`} w-[25px] h-[25px]  text-primaryColor cursor-pointer fixed top-[100px]`}
+              onClick={() => setToggle(prevState => !prevState)}
+            />
+            <div className={`bg-[rgba(255,255,255,0.05)] p-4 `}>
+            <div className={`${toggle ? `block` : `hidden`} relative mt-10 w-full mb-10 md:hidden`}>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Search for anything"
+              className={`border-[1px] border-primaryColor p-4 outline-none text-textColor rounded-[8px] h-[40px] bg-transparent text-[10px] md:text-[14px] leading-[16px] w-full font-Work Sans`}
+            />
+            <div
+              className={`absolute w-[56px] bg-secondaryColor right-0 h-[40px] flex justify-center items-center top-0 rounded-tr-[8px] rounded-br-[8px]`}
+            >
+              <BsSearch
+                className={`w-[14px] h-[14px] text-white cursor-pointer`}
+              />
+            </div>
+          </div>
+              <div className={`flex items-center my-10`}>
                 <Link to="/" className={`flex items-center`}>
                   <img src={Briefcase} alt="" />
                   <p
-                    className={`font-Work Sans text-primaryColor text-[16px] leading-[19px] ml-2`}
+                    className={`${toggle ? `block` : `hidden`} font-Work Sans text-primaryColor text-[16px] leading-[19px] ml-2`}
                   >
                     Switch Organization
                   </p>
@@ -84,7 +105,7 @@ const Dashboard = () => {
                 <Link to="/" className={`flex items-center`}>
                   <img src={Home} alt="" />
                   <p
-                    className={`font-Work Sans text-primaryColor text-[16px] leading-[19px] ml-2`}
+                    className={`${toggle ? `block` : `hidden`} font-Work Sans text-primaryColor text-[16px] leading-[19px] ml-2`}
                   >
                     Dashboard
                   </p>
@@ -92,7 +113,7 @@ const Dashboard = () => {
               </div>
               <div>
                 <p
-                  className={`uppercase text-textColor font-[500] text-[12px] leading-[14px] mb-6`}
+                  className={`${toggle ? `text-[12px]` : `text-[.4rem]`} uppercase text-textColor font-[500] leading-[14px] mb-6`}
                 >
                   Customers
                 </p>
@@ -104,7 +125,7 @@ const Dashboard = () => {
                       <Link to="/" className={`flex items-center`}>
                         <img src={data.img} alt="" />
                         <p
-                          className={`font-Work Sans text-primaryColor text-[16px] leading-[19px] ml-2`}
+                          className={`${toggle ? `block` : `hidden`} font-Work Sans text-primaryColor text-[16px] leading-[19px] ml-2`}
                         >
                           {data.link}
                         </p>
@@ -116,7 +137,7 @@ const Dashboard = () => {
 
               <div>
                 <p
-                  className={`uppercase text-textColor font-[500] text-[12px] leading-[14px] mb-6`}
+                  className={`${toggle ? `text-[12px]` : `text-[.4rem]`} uppercase text-textColor font-[500] text-[12px] leading-[14px] mb-6`}
                 >
                   Businesses
                 </p>
@@ -128,7 +149,7 @@ const Dashboard = () => {
                       <Link to="/" className={`flex items-center`}>
                         <img src={data.img} alt="" />
                         <p
-                          className={`font-Work Sans text-primaryColor text-[16px] leading-[19px] ml-2`}
+                          className={`${toggle ? `block` : `hidden`} font-Work Sans text-primaryColor text-[16px] leading-[19px] ml-2`}
                         >
                           {data.link}
                         </p>
@@ -139,7 +160,7 @@ const Dashboard = () => {
               </div>
               <div>
                 <p
-                  className={`uppercase text-textColor font-[500] text-[12px] leading-[14px] mb-6`}
+                  className={`${toggle ? `text-[12px]` : `text-[.4rem]`} uppercase text-textColor font-[500] text-[12px] leading-[14px] mb-6`}
                 >
                   Settings
                 </p>
@@ -151,7 +172,7 @@ const Dashboard = () => {
                       <Link to="/" className={`flex items-center`}>
                         <img src={data.img} alt="" />
                         <p
-                          className={`font-Work Sans text-primaryColor text-[16px] leading-[19px] ml-2`}
+                          className={`${toggle ? `block` : `hidden`} font-Work Sans text-primaryColor text-[16px] leading-[19px] ml-2`}
                         >
                           {data.link}
                         </p>
@@ -162,7 +183,6 @@ const Dashboard = () => {
               </div>
             </div>
           </div>
-        </div>
       </div>
     </section>
   );
