48235e13e6e5acf7f93923f1c7373da609a0c012
Gloria <ubahgloria16@gmail.com>
2023-05-18 12:29:26 +0100

    Chore: added user details  template

--- LendSqr/src/components/Userdetails.tsx
+++ LendSqr/src/components/Userdetails.tsx
@@ -6,14 +6,14 @@ const UserDetails = ({...props}) => {
      
 
      return (
-          <section className={`${toggle ? `${style._section_width_resize}` : `${style._section_width}`} mt-[100px] ml-auto bg-[#fbfbfb] relative -z-10 p-6 md:p-[3rem]`}>
+          <section className={`${toggle ? `${style._section_width_resize}` : `${style._section_width}`} mt-[100px] ml-auto bg-[#fbfbfb] relative  p-6 md:p-[3rem]`}>
                <div>
                     <div className={``}>
                          <p className={`text-primaryColor font-Work Sans font-[500] text-[24px] leading-[28px] mb-10`}>Users</p>
                     </div>
                <div className={`grid md:grid-cols-2 gap-8 lg:grid-cols-4 xl:grid-cols-4 mb-8`}>
                     {UserInfo.map(info => {
-                         return (<div className={`bg-[#fff] rounded-[4px] shadow-md border-[1px] border-[rgba(125, 125, 125, 0.06)] p-4  w-full`} key={info.title}>
+                         return (<div className={`bg-[#fff] rounded-[4px] shadow-md border-[1px] border-[rgba(125, 125, 125, 0.06)] p-4  w-full hover:bg-[#f3efef] cursor-pointer`} key={info.title}>
                     <img src={info.img} alt="" className={`mb-4`} />
                               <p className={`uppercase text-textColor font-Work Sans font-[500] text-[14px] leading-[16px] mb-4`}>{info.title}</p>
                               <p className={`uppercase text-primaryColor font-Work Sans font-[600] text-284px] leading-[16px]`}>{info.count}</p>
@@ -21,13 +21,19 @@ const UserDetails = ({...props}) => {
                </div>
 
                <div className={`${style._content} bg-[#fff] rounded-[4px] shadow-md border-[1px] border-[rgba(125, 125, 125, 0.06)] p-4`}>
-                         <div className={`w-[]`}>
-                              <div className={`flex justify-between items-center`}>
+                         <div className={`w-[1000px]`}>
+                              <div className={`flex justify-between relative`}>
                                    {tableHeading.map(heading => {
-                                        return <div className={`flex items-center`} key={heading.header}>
-                                             <p className={`font-Work Sans font-[600] text-[12px] leading-[14px] uppercase text-textColor`}>{heading.header}</p>
-                                        <img src={heading.icon} alt="" className={`ml-2`} />
-                                   </div>
+                                        return <div className={``}>
+                                             <div className={`flex items-center`} key={heading.header}>
+                                                  <p className={`font-Work Sans font-[600] text-[12px] leading-[14px] uppercase text-textColor`}>{heading.header}</p>
+                                                  <img src={heading.icon} alt="" className={`ml-2`} />
+                                             </div>
+                                             <div className={`py-6 `}>
+                                                  <p>{heading.details}</p>
+                                             </div>
+                                             <div className={`  bg-[#766f6f]`}></div>
+                                        </div>
                                    })}
                               </div>
                               
--- LendSqr/src/lib/tableHeading.tsx
+++ LendSqr/src/lib/tableHeading.tsx
@@ -2,27 +2,34 @@ import Icon from "../assets/Vector (3).svg";
 const Heading = [
        {
           header: "Organization",
-          icon: Icon
+          icon: Icon,
+          details: "Lendsqr"
        },
        {
           header: "Username",
-          icon: Icon
+          icon: Icon,
+          details: "Adedeji"
           },
        {
           header: "Email",
-          icon: Icon
+          icon: Icon,
+          details: "adedeji@lendsqr.com"
        },
        {
           header: "Phone Number",
-          icon: Icon
+          icon: Icon,
+          details: "08078903721"
        },
        {
           header: "Date Joined",
-          icon: Icon
+          icon: Icon,
+          details: "May 15, 2020 10:00 AM"
        },
        {
           header: "Status",
-          icon: Icon
+          icon: Icon,
+          details: "Inactive"
+
        },
   ]
 
