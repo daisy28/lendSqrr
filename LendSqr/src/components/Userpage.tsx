77b45350f5905d9a969f9d4ca81fdb32bc554f10
Gloria <ubahgloria16@gmail.com>
2023-06-10 16:30:11 +0100

    Feat: Data management for full user info.

--- LendSqr/src/components/Userpage.tsx
+++ LendSqr/src/components/Userpage.tsx
@@ -1,7 +1,11 @@
+import { useState, useContext } from "react";
+import style from "./style.module.css";
 
-const UserPage = () => {
+const UserPage = ({ ...props }) => {
+  const { fullUserInfo }  = props
+  console.log(fullUserInfo)
   return (
-    <section>
+    <section className={`${style._section_width} mt-[100px] ml-auto bg-[#fbfbfb] relative p-6 md:p-[3rem]`}>
       Display complete user details here
     </section>
   )
